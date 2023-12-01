a97b15fbd3cc2c0cecd1059fccec39245e1635d9
deepana0908 <145099043+deepana0908@users.noreply.github.com>
2023-11-30 18:06:40 -0600

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import styled from "styled-components";
-import loginImg from "../images/login-im11jpeg.jpeg";
+import loginImg from "../images/abc.jpg";
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
 
